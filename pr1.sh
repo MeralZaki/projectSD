5d9afeed7ae5ed5c5c09190db316fab8b6152785
MeralZaki <s444000552@st.uqu.edu.sa>
2024-05-18 18:09:47 +0300

    dana

---
+++ pr1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Movies currently showing stored in a file
+MOVIE_FILE="movies.txt"
+
+# Showtimes stored in another file
+SHOWTIMES_FILE="showtimes.txt"
+
+# Bookings stored in another file
+BOOKINGS_FILE="bookings.txt"
